1e8e6a6149abefeed2ff92658dc71732e54bb1d7
meganglass <69376476+meganglass@users.noreply.github.com>
2020-11-15 13:58:18 +1100

    Add files via upload

---
+++ transform.js
@@ -0,0 +1,28 @@
+console.log("script start")
+
+//const signature = document.querySelector(".signature_item")
+const selectionItems = document.querySelectorAll(".selection_item")
+
+
+window.addEventListener('DOMContentLoaded', function() {
+  
+  console.log("loaded")
+
+  window.addEventListener("scroll", function () {
+    console.log("event start")
+
+    const pixels = window.scrollY
+
+    selectionItems.forEach((item, index) => {
+      let movement = (pixels * index) / 50
+
+      if (index % 2 == 1) {
+        movement = movement * -1
+      }
+
+      item.style.transform = `translateX(${-1 * movement}px)`
+    })
+
+  })
+  
+});
