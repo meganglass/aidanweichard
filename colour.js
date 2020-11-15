776ef596446f0026601ba63230eda90324e9415d
meganglass <69376476+meganglass@users.noreply.github.com>
2020-11-15 13:56:52 +1100

    Add files via upload

---
+++ colour.js
@@ -0,0 +1,34 @@
+window.addEventListener('DOMContentLoaded', function() {
+    console.log("loaded")
+      window.addEventListener("scroll", function () {
+        console.log("event start")
+
+        const pixels = window.scrollX
+        const end = window.documentElement.scrollWidth
+        const remaining = end - window.innerWidth
+
+        const percentage = (pixels / remaining) * 100
+
+        console.log(percentage)
+
+        if (percentage < 20) {
+          signature.style.fill = "yellow"
+        }
+
+        if (percentage >= 20 && percentage <= 40) {
+          signature.style.fill = "green"
+        }
+
+        if (percentage >= 40 && percentage <= 60) {
+          signature.style.fill = "pink"
+        }
+
+        if (percentage >= 60 && percentage <= 80) {
+          signature.style.fill = "blue"
+        }
+
+        if (percentage >= 80 && percentage <= 100) {
+          signature.style.fill = "red"
+        }
+    }) 
+  });
